3b228e1700f87d272b10a3a2248c8b92b1b2c804
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2020-03-28 23:17:59 +0300

    General TODOs are listed

--- CoronaKiller-Backend/src/main/java/com/coronakiller/Main.java
+++ CoronaKiller-Backend/src/main/java/com/coronakiller/Main.java
@@ -5,9 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing // Needed for enabling the SpringBoot's JPA Auditing
+@EnableJpaAuditing
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 }
+
+/**
+ * TODO:
+ *  - Finish Impl of Cont-Service-Repo => ALPER + YAVUZ
+ *  - Security => YAVUZ
+ *  - Unit Testing + PROD DB PREPARATION => ALPER
+ *  - Swagger + README + DB DESIGN => YAVUZ
+ *  - Postman Collections => YAVUZ + ALPER
+ */
