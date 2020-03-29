233304412e740e650425bf3b1f65f326991fb144
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2020-03-30 00:56:22 +0300

    Swagger mounted to the project

---
+++ CoronaKiller-Backend/src/main/java/com/coronakiller/Config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.coronakiller.Config;
+
+
+import static springfox.documentation.builders.PathSelectors.regex;
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors
+						.basePackage("com.coronakiller.Controller"))
+				.paths(PathSelectors.regex("/.*"))
+				.build().apiInfo(apiEndPointsInfo());
+	}
+	private ApiInfo apiEndPointsInfo() {
+		return new ApiInfoBuilder().title("Corona Killer REST API")
+				.description("Documentation of CoronaKiller Game's Backend endpoints")
+				.contact(new Contact("Yavuz Selim Yesilyurt", "user.ceng.metu.edu.tr/~e2259166", "yesilyurt.selim@metu.edu.tr"))
+				.license("MIT")
+				.licenseUrl("https://opensource.org/licenses/MIT")
+				.version("1.0.0")
+				.build();
+	}
+}
