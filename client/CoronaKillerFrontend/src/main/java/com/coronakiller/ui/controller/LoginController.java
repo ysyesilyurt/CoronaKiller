4193cab50d555c3280dbaeaeba3d14145525f0aa
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2020-04-24 21:04:44 +0300

    Created a dummy dashboard and tested page switches

--- client/CoronaKillerFrontend/src/main/java/com/coronakiller/ui/controller/LoginController.java
+++ client/CoronaKillerFrontend/src/main/java/com/coronakiller/ui/controller/LoginController.java
@@ -1,6 +1,18 @@
 package com.coronakiller.ui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class LoginController {
@@ -11,7 +23,18 @@ public class LoginController {
 //	@FXML
 //	private PasswordField passwordField;
 //
-//	@FXML
-//	private Button loginButton;
+	@FXML
+	private Button loginButton;
 
+	@Value("classpath:/fxml/dashboard.fxml")
+	private Resource dashboardResource;
+
+	@FXML
+	protected void handleLoginButtonAction(ActionEvent event) throws IOException {
+		Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Parent registerPage = FXMLLoader.load(dashboardResource.getURL());
+		Scene scene = new Scene(registerPage, 600, 800);
+		currentStage.setScene(scene);
+		currentStage.show();
+	}
 }
