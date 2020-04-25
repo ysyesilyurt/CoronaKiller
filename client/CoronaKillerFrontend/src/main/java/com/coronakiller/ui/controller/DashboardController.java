93074d7dc288024bb593ee064e9060ea4d7c2edd
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2020-04-26 02:37:54 +0300

    Dashboard UI Implementd

--- client/CoronaKillerFrontend/src/main/java/com/coronakiller/ui/controller/DashboardController.java
+++ client/CoronaKillerFrontend/src/main/java/com/coronakiller/ui/controller/DashboardController.java
@@ -1,5 +1,6 @@
 package com.coronakiller.ui.controller;
 
+import com.coronakiller.ui.application.StageInitializer;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,6 +8,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.springframework.stereotype.Component;
 
@@ -16,10 +19,44 @@ import java.io.IOException;
 public class DashboardController {
 
 	@FXML
+	public AnchorPane dashboardPane;
+	@FXML
+	public AnchorPane innerPane;
+	@FXML
+	public JFXButton continueGameButton;
+	@FXML
+	public JFXButton newGameButton;
+	@FXML
+	public JFXButton leaderboardButton;
+	@FXML
+	public Text snackbarContent;
+	@FXML
+	public Text totalScore;
+	@FXML
+	public Text username;
+	@FXML
 	private JFXButton leaderBoardButton;
 
 	@FXML
-	public void handleLeaderBoardButtonAction(ActionEvent event)throws IOException {
+	public void initialize() {
+		dashboardPane.getStylesheets().add("css/styles.css");
+		username.setText(String.format("Welcome %s!", StageInitializer.currentPlayer.getUsername()));
+		totalScore.setText(String.format("Your Total Score: %s", StageInitializer.currentPlayer.getTotalScore()));
+		continueGameButton.setDisable(StageInitializer.currentPlayer.getGameSessionId() == null);
+	}
+
+	@FXML
+	public void onClickContinueGame(ActionEvent event) {
+
+	}
+
+	@FXML
+	public void onClickNewGame(ActionEvent event) {
+
+	}
+
+	@FXML
+	public void onClickGoToLeaderboard(ActionEvent event) throws IOException {
 		Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		Parent leaderBoardPage = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/leaderBoard.fxml"));
 		Scene scene = new Scene(leaderBoardPage, 600, 800);
