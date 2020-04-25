134f236f9632d94588b7417d4a6f2c461fc982d9
AlperKocaman <alperkman@gmail.com>
2020-04-25 18:05:06 +0300

    Added leaderboard button to dashboard for testing purposes

--- client/CoronaKillerFrontend/src/main/java/com/coronakiller/ui/controller/DashboardController.java
+++ client/CoronaKillerFrontend/src/main/java/com/coronakiller/ui/controller/DashboardController.java
@@ -1,7 +1,31 @@
 package com.coronakiller.ui.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class DashboardController {
+
+	@FXML
+	private JFXButton leaderBoardButton;
+
+	@FXML
+	public void handleLeaderBoardButtonAction(ActionEvent event)throws IOException {
+		Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/fxml/leaderBoard.fxml"));
+		Parent leaderBoardPage = loader.load();
+		Scene scene = new Scene(leaderBoardPage, 600, 800);
+		currentStage.setScene(scene);
+		currentStage.show();
+	}
 }
