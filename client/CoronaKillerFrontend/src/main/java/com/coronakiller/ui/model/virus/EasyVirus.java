d5459495e8e9256f1b515816eff48e314dd18e74
AlperKocaman <alperkman@gmail.com>
2020-04-26 17:55:29 +0300

    Virus and one of its type introduced.

---
+++ client/CoronaKillerFrontend/src/main/java/com/coronakiller/ui/model/virus/EasyVirus.java
@@ -0,0 +1,47 @@
+package com.coronakiller.ui.model.virus;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+import lombok.Getter;
+import lombok.Setter;
+
+import static com.coronakiller.ui.constants.UiConstants.*;
+
+@Getter
+@Setter
+/**
+ * This class represents the most basic virus(alien) type.
+ */
+public class EasyVirus extends Virus {
+
+	private int virusHealth;
+	private Timeline easyVirusTimeline;
+
+	public EasyVirus(double xPosition, double yPosition, int width, int height) {
+		super(xPosition, yPosition, width, height, EASY_VIRUS_HEALTH);
+	}
+
+	@Override
+	public void virusAutoFire(Pane pane) {
+
+	}
+
+	@Override
+	public void virusAutoMove() {
+		this.easyVirusTimeline = new Timeline(
+				new KeyFrame( Duration.millis(10), e ->{
+					this.setX(this.getX()- EASY_VIRUS_VELOCITY);
+				})
+		);
+		this.easyVirusTimeline.setCycleCount(Timeline.INDEFINITE);
+		this.easyVirusTimeline.play();
+	}
+
+	@Override
+	public void stopFireAndMove() {
+
+	}
+}
---
+++ client/CoronaKillerFrontend/src/main/java/com/coronakiller/ui/model/virus/Virus.java
@@ -0,0 +1,30 @@
+package com.coronakiller.ui.model.virus;
+
+import javafx.animation.Timeline;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class Virus extends Rectangle {
+
+	private int virusHealth;
+
+	public Virus(double xPosition, double yPosition, int width, int height, int virusHealth) {
+		super(xPosition, yPosition, width, height);
+		this.virusHealth = virusHealth;
+	}
+
+	public void getShot(int bulletDamage){
+		this.virusHealth-= bulletDamage;
+	}
+
+	public abstract void virusAutoFire(Pane pane);
+
+	public abstract void virusAutoMove();
+
+	public abstract void stopFireAndMove();
+
+}
