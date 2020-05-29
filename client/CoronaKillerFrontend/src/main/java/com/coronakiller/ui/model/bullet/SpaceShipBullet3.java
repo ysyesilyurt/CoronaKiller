78dd40ca323430b72d2f9961d1f2a27b59576e82
AlperKocaman <alperkman@gmail.com>
2020-05-29 21:12:35 +0300

    Added SpaceShipBullet3 class for score update

---
+++ client/CoronaKillerFrontend/src/main/java/com/coronakiller/ui/model/bullet/SpaceShipBullet3.java
@@ -0,0 +1,52 @@
+package com.coronakiller.ui.model.bullet;
+
+import com.coronakiller.ui.controller.level.GameLevel1Controller;
+import com.coronakiller.ui.controller.level.GameLevelController;
+import com.coronakiller.ui.model.virus.Virus;
+import javafx.animation.Timeline;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.ImagePattern;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import static com.coronakiller.ui.constants.GameConstants.*;
+
+public class SpaceShipBullet3 extends SpaceShipBullet{
+
+	Image spaceshipBullet3Icon = new Image(SPACESHIP_BULLET3_ICON_URL);
+
+	/**
+	 * Constructor method of second type of bullet.
+	 * @param xPosition
+	 * @param yPosition
+	 */
+	public SpaceShipBullet3(double xPosition, double yPosition){
+		super(xPosition, yPosition, SPACESHIP_BULLET3_WIDTH, SPACESHIP_BULLET3_HEIGHT, SPACESHIP_BULLET3_DAMAGE, SPACESHIP_BULLET3_VELOCITY);
+	}
+
+	@Override
+	public void checkCollision(Pane currentPane) throws IOException {
+		Virus virus = GameLevel1Controller.levelViruses.get(0);
+		if(virus.getBoundsInParent().intersects(this.getBoundsInParent())){
+			this.getBulletTimeline().stop();
+			currentPane.getChildren().remove(this);
+			virus.getShot(this.getBulletDamage());
+			GameLevelController.currentSessionScore += virus.getPointsFromVirus();
+			GameLevelController.updateScoreValue();
+			if(virus.getVirusHealth() <= 0){
+				virus.stopFireAndMove();
+				currentPane.getChildren().remove(virus);
+				if(!GameLevelController.isGameLevelFinished) {
+					GameLevelController.finishLevelSuccessfully();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void changeIconOfBullet() {
+		this.setFill(new ImagePattern(spaceshipBullet3Icon));
+	}
+}
