81b06ebf3cd98fba33dee9bdaf9430f5fea1f159
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2020-06-04 22:09:27 +0300

    poms are updated in accordance with deployment configurations

--- server/CoronaKillerBackend/src/main/java/com/coronakiller/Main.java
+++ server/CoronaKillerBackend/src/main/java/com/coronakiller/Main.java
@@ -2,11 +2,12 @@ package com.coronakiller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class Main {
+public class Main extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
